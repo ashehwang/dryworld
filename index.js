e7e42aa99331c62f70d8c8ceee8228fea86974bb
Ashe <ashe@nyu.edu>
2020-07-16 17:53:46 -0700

    able to render different graphs depending on filter; world map enabled

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const svg = d3.select("svg");
-
-// const width = +svg.attr("width");
-// const height = +svg.attr("height");
+const svg = d3.select("svg.dry-map");
 
 // const projection = d3.geoNaturalEarth1();
 const projection = d3.geoEckert4();
@@ -11,7 +8,7 @@ const g = svg.append('g')
 
 g.append('path')
     .attr('class', 'sphere')
-    .attr('d', pathGenerator({type: 'Sphere'}))
+    // .attr('d', pathGenerator({type: 'Sphere'}))
 
 svg.call(d3.zoom().on('zoom', () => {
     g.attr('transform', d3.event.transform)
@@ -22,13 +19,16 @@ const colorValue = d => d.properties.risk
 
 Promise.all([
     // d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv'),
-    d3.tsv('./src/waterdata.tsv'),
+    // d3.tsv('./src/waterdata.tsv'),
+    d3.tsv('./src/waterdata2.tsv'),
     d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
 ]).then(([tsvData, topoJSONdata]) => {
         const rowById = {};
         tsvData.forEach( d => {
             rowById[d.iso_n3] = d;
         })
+
+        console.log(topoJSONdata)
 
         const countries = topojson.feature(topoJSONdata, topoJSONdata.objects.countries)
 
@@ -38,8 +38,8 @@ Promise.all([
 
         colorScale.domain(countries.features.map(colorValue))
             .domain(colorScale.domain().sort().reverse())
-            // .range(d3.schemeSpectral[colorScale.domain().length])
             .range(d3.schemeYlOrRd[colorScale.domain().length])
+            // .range(d3.schemeYlGn[colorScale.domain().length])
             
         console.log(colorScale.domain().sort())
 
@@ -50,5 +50,70 @@ Promise.all([
                 .attr('d', pathGenerator)
                 .attr('fill', d => colorScale(colorValue(d)))
             .append('title')
-                .text(d => d.properties.name + ': ' + colorValue(d))
+                .text(d => d.properties.name + ': ' + colorValue(d) + ' Risk')
 })
+
+const svg2 = d3.select('svg.water-bars')
+// svg2.style('background-color', 'red')
+
+const width = +svg2.attr("width");
+const height = +svg2.attr("height");
+
+const render = (data, filter) => {
+    const margin = { top: 20, right: 20, bottom: 20, left: 80 };
+    const innerHeight = height - margin.top - margin.bottom;
+    const innerWidth = width - margin.left - margin.right;
+    const yValue = d => d.water;
+    const xValue = d => d.action;
+
+    const filteredData = data.filter( d => d.filter === filter)
+
+    const xScale = d3.scaleBand()
+        // .domain(data.filter( d => d.filter === filter).map(xValue))
+        // .domain(data.map(xValue))
+        .domain(filteredData.map(xValue))
+        .range([0, innerWidth])
+        .padding(0.1);
+
+    console.log(xScale.domain())
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(filteredData, yValue)])
+        // .domain([0, d3.max(data, yValue)])
+        // .domain([0, data.filter( d => d.filter === filter).d3.max(yValue)])
+        .range([innerHeight, 0]);
+
+    const g = svg2.append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    g.append('g').call(d3.axisLeft(yScale))
+    // .tickSize(10)
+    // .tickFormat('.3s')
+    // .select('.domain')
+    // .remove();
+
+    g.append("g")
+        .call(d3.axisBottom(xScale))
+        .attr("transform", `translate(0, ${innerHeight})`);
+    //   .append('text')
+    //     .attr('y', 0)
+    //     .attr('fill', 'black')
+    //     .text('water spent in gallons')
+    // .selectAll('.domain, .tick line')
+    // .remove();
+
+    g.selectAll('rect').data(filteredData)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => xScale(d.action))
+        .attr('y', d => yScale(d.water))
+        .attr('width', xScale.bandwidth())
+        .attr('height', d => innerHeight - yScale(d.water));
+};
+
+d3.csv('./src/data.csv').then(data => {
+    data.forEach(d => {
+        d.water = +d.water;
+    });
+    render(data, "food");
+});
